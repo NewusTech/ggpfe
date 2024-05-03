4e7f709d95969003566cd63fbd93ca3fc545423a
muttaqin0708 <muttaqin0708@gmail.com>
2024-05-03 20:15:06 +0700

    GGPR-77 dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,103 +3,103 @@ import { onMounted, reactive, ref } from 'vue';
 import { apiBaseUrl } from '@/config/apiConfig';
 
 const datamasters = ref([]);
-// const lineData = reactive({
-//     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
-//     datasets: [
-//         {
-//             label: 'This Year',
-//             data: [],
-//             fill: false,
-//             backgroundColor: '#000000',
-//             borderColor: '#000000',
-//             tension: 0.4,
-//         },
-//         {
-//             label: 'Last Year',
-//             data: [],
-//             borderDash: [5, 5],
-//             fill: false,
-//             backgroundColor: '#c0c0c0',
-//             borderColor: '#c0c0c0',
-//             tension: 0.4,
-//         }
-//     ]
-// });
-
-// const lineData2 = reactive({
-//     labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
-//     datasets: [
-//         {
-//             label: 'This Year',
-//             data: [],
-//             fill: false,
-//             backgroundColor: '#000000',
-//             borderColor: '#000000',
-//             tension: 0.4,
-//         },
-//         {
-//             label: 'Last Year',
-//             data: [],
-//             borderDash: [5, 5],
-//             fill: false,
-//             backgroundColor: '#c0c0c0',
-//             borderColor: '#c0c0c0',
-//             tension: 0.4,
-//         }
-//     ]
-// });
-
-// const lineOptions = ref({
-//     plugins: {
-//         legend: {
-//             align: 'end',
-//             labels: {
-//                 usePointStyle: true,
-//                 pointStyle: 'circle',
-//                 boxWidth: 4, 
-//             }
-//         }
-//     }
-// });
-
-onMounted( async () => {
-    // try {
-    //     // Ambil data dari Laravel API
-    //     const response = await fetch(`${apiBaseUrl}/api/dashboard`, {
-    //         method: 'GET',
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //             Authorization: `Bearer ${localStorage.getItem('token')}`
-    //         }
-    //     });
-
-    //     const data = await response.json();
-
-    //     if (response.ok) {
-    //         datamasters.value = data.data;
-    //         console.log(datamasters)
-    //         updateChartData();
-    //     } else {
-    //         throw new Error(data.error || 'Failed to fetch data from API');
-    //     }
-    // } catch (error) {
-    //     console.error('Error fetching data from API:', error);
-    //     toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to fetch data from API', life: 5000 });
-    // }
+const lineData = reactive({
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+    datasets: [
+        {
+            label: 'This Year',
+            data: [],
+            fill: false,
+            backgroundColor: '#000000',
+            borderColor: '#000000',
+            tension: 0.4,
+        },
+        {
+            label: 'Last Year',
+            data: [],
+            borderDash: [5, 5],
+            fill: false,
+            backgroundColor: '#c0c0c0',
+            borderColor: '#c0c0c0',
+            tension: 0.4,
+        }
+    ]
 });
 
-// function updateChartData() {
-//     lineData.datasets[0].data = datamasters.value.total_inbound_this_year;
-//     lineData.datasets[1].data = datamasters.value.total_inbound_last_year;
-//     lineData2.datasets[0].data = datamasters.value.total_outbound_this_year;
-//     lineData2.datasets[1].data = datamasters.value.total_outbound_last_year;
-// }
+const lineData2 = reactive({
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Agu', 'Sep', 'Okt', 'Nov', 'Des'],
+    datasets: [
+        {
+            label: 'This Year',
+            data: [],
+            fill: false,
+            backgroundColor: '#000000',
+            borderColor: '#000000',
+            tension: 0.4,
+        },
+        {
+            label: 'Last Year',
+            data: [],
+            borderDash: [5, 5],
+            fill: false,
+            backgroundColor: '#c0c0c0',
+            borderColor: '#c0c0c0',
+            tension: 0.4,
+        }
+    ]
+});
+
+const lineOptions = ref({
+    plugins: {
+        legend: {
+            align: 'end',
+            labels: {
+                usePointStyle: true,
+                pointStyle: 'circle',
+                boxWidth: 4, 
+            }
+        }
+    }
+});
+
+onMounted( async () => {
+    try {
+        // Ambil data dari Laravel API
+        const response = await fetch(`${apiBaseUrl}/api/dashboard`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        });
+
+        const data = await response.json();
+
+        if (response.ok) {
+            datamasters.value = data.data;
+            console.log(datamasters)
+            updateChartData();
+        } else {
+            throw new Error(data.error || 'Failed to fetch data from API');
+        }
+    } catch (error) {
+        console.error('Error fetching data from API:', error);
+        toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to fetch data from API', life: 5000 });
+    }
+});
+
+function updateChartData() {
+    lineData.datasets[0].data = datamasters.value.total_inbound_this_year;
+    lineData.datasets[1].data = datamasters.value.total_inbound_last_year;
+    lineData2.datasets[0].data = datamasters.value.total_outbound_this_year;
+    lineData2.datasets[1].data = datamasters.value.total_outbound_last_year;
+}
 
 </script>
 
 <template>
     <div class="grid">
-        <!-- <div class="col-12 lg:col-6 xl:col-3">
+        <div class="col-12 lg:col-6 xl:col-3">
             <div class="card mb-0">
                 <div class="flex justify-content-between mb-3">
                     <div>
@@ -151,6 +151,6 @@ onMounted( async () => {
                 <h5>Outbound</h5>
                 <Chart type="line" :data="lineData2" :options="lineOptions" />
             </div>
-        </div> -->
+        </div>
     </div>
 </template>
